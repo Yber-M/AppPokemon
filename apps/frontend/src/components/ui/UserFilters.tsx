b3d5302751701a46ef64b06c4153934cb29b680d
Yb-M <ybermarlon@gmail.com>
2026-01-23 00:53:28 -0500

    ADD: se agrega un filtro por roles y tambien un input para buscar usuarios

--- apps/frontend/src/components/ui/UserFilters.tsx
+++ apps/frontend/src/components/ui/UserFilters.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Input } from "@/src/components/ui/Input";
-import { IoCloseCircleOutline } from "react-icons/io5";
+import { MdCleaningServices } from "react-icons/md";
+
 import clsx from "clsx";
 
 type UserFiltersProps = {
@@ -77,7 +78,7 @@ export function UserFilters({ search, role, onSearchChange, onRoleChange, onClea
         </div>
         <button
           type="button"
-          className="flex cursor-pointer items-center gap-2 rounded-xl border border-white/15 bg-white/5 px-3 py-2 text-xs font-semibold text-white transition hover:border-white/30 hover:bg-white/10"
+          className="flex cursor-pointer items-center gap-2 rounded-xl border border-white/15 bg-white/5 px-3 py-2.5 text-xs font-semibold text-white transition hover:border-white/30 hover:bg-white/10"
           onClick={() => {
             onSearchChange("");
             onRoleChange("ALL");
@@ -85,7 +86,7 @@ export function UserFilters({ search, role, onSearchChange, onRoleChange, onClea
             setOpen(false);
           }}
         >
-          <IoCloseCircleOutline />
+          <MdCleaningServices className="text-lg" />
           Limpiar filtros
         </button>
       </div>
